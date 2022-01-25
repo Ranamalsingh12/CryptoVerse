b791a83fe4e5470c14426d845b0849e7868679d7
ranamalsingh12 <ranams99911@gmail.com>
2022-01-25 13:45:14 +0530

    creating Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, CryptoDetails, News, Homepage, Exchanges, Cryptocurrencies } from './components';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                {/* : means its dynamic */}
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
