a8340abb6b0ea3a50dfdddfd288d69175fd278f6
ranamalsingh12 <ranams99911@gmail.com>
2022-01-28 13:14:41 +0530

    cryptocurrencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
+import { Cryptocurrencies, News } from '../components';
 
-import { useGetCryptosQuery } from '../services/cryptoApi.js'
+import { useGetCryptosQuery } from '../services/cryptoApi.js';
 
 const { Title } = Typography;
 
 const Homepage = () => {
 
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
   // console.log(data);
 
@@ -37,10 +38,18 @@ const Homepage = () => {
       <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
       </Col>
     </Row>
+
     <div className="home-heading-container">
       <Title level={2} className="home-title" >Top 10 Cryptocurrencies in the world</Title>
       <Title level={2} className="show-more" ><Link to="/cryptocurrencies">Show More</Link></Title>
     </div>
+    <Cryptocurrencies simplified />
+
+    <div className="home-heading-container">
+      <Title level={2} className="home-title" >Latest Crypto News</Title>
+      <Title level={2} className="show-more" ><Link to="/news">Show More</Link></Title>
+    </div>
+    <News simplified />
   </>
 );
 };
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const News = () => {
   return (
   <div>
-      
+      News
   </div>
 );
 };
