051ed3982a1d0119257be7d47b6e58c8e1dff050
ranamalsingh12 <ranams99911@gmail.com>
2022-01-30 12:46:25 +0530

    chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -5,6 +5,41 @@ import { Col, Row, Typography } from 'antd';
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName}) => {
+  // const coinPrice = [];
+  // const coinTimestamp = [];
+
+  // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+  //   coinPrice.push(coinHistory?.data?.history[i].price);
+  // }
+
+  // for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+  //   coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  // }
+  // const data = {
+  //   labels: coinTimestamp,
+  //   datasets: [
+  //     {
+  //       label: 'Price In USD',
+  //       data: coinPrice,
+  //       fill: false,
+  //       backgroundColor: '#0071bd',
+  //       borderColor: '#0071bd',
+  //     },
+  //   ],
+  // };
+
+  // const options = {
+  //   scales: {
+  //     yAxes: [
+  //       {
+  //         ticks: {
+  //           beginAtZero: true,
+  //         },
+  //       },
+  //     ],
+  //   },
+  // };
+
   return (
   <>
       <Row className='chart-header'>
@@ -20,6 +55,8 @@ const LineChart = ({ coinHistory, currentPrice, coinName}) => {
             </Title>
           </Col>
       </Row>
+
+      {/* <Line data={data} options={options} /> */}
   </>
 );
 };
