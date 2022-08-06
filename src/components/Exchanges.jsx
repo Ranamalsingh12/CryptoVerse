c140f2615fa07c4207f24f01d3662ad5f23864fb
ranamalsingh12 <ranams99911@gmail.com>
2022-08-07 01:19:13 +0530

    exchange changes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+// import millify from 'millify';
+import { Row, Col } from 'antd';
+// import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
-const { Text } = Typography;
-const { Panel } = Collapse;
+// const { Text } = Typography;
+// const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
+  const { isFetching } = useGetExchangesQuery();
+  // const exchangesList = data?.data?.exchanges;
   if (isFetching) return <Loader />;
 
   return (
@@ -21,8 +21,6 @@ const Exchanges = () => {
         <Col span={6}>24h Trade Volume</Col>
         <Col span={6}>Markets</Col>
         <Col span={6}>Change</Col>
-      </Row>
-      <Row>
       </Row>
     </>
   );
